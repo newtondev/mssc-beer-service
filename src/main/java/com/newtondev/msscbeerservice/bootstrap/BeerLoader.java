ffeebc70bc916a2ce41c0e2e472255d8e13ff4fd
Craig Newton <craig@hyvemobile.co.za>
2020-02-13 21:46:19 +0100

    Added validation closes #8

--- src/main/java/com/newtondev/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/newtondev/msscbeerservice/bootstrap/BeerLoader.java
@@ -17,7 +17,7 @@ public class BeerLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         loadBeerObjects();
     }
 
